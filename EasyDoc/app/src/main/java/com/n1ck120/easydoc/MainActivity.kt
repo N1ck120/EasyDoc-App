8817b62a19601e9d27c875159fa5bd8d39584875
N1ck120 <nickolasaugusto74@gmail.com>
2025-03-20 00:12:19 -0300

    Check if all fields are filled

--- EasyDoc/app/src/main/java/com/n1ck120/easydoc/MainActivity.kt
+++ EasyDoc/app/src/main/java/com/n1ck120/easydoc/MainActivity.kt
@@ -4,18 +4,17 @@ import android.app.AlertDialog
 import android.os.Bundle
 import android.os.Environment
 import android.view.LayoutInflater
-import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.textfield.TextInputEditText
 import com.itextpdf.kernel.pdf.PdfWriter
 import com.itextpdf.kernel.pdf.*
 import java.io.File
 import com.itextpdf.layout.Document
-import com.itextpdf.layout.element.Image
 import com.itextpdf.layout.element.Paragraph
 
 class MainActivity : AppCompatActivity() {
@@ -29,48 +28,53 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         val createDoc = findViewById<FloatingActionButton>(R.id.floatingActionButton)
-
-        /*fun contentVer(){
-            if (){
-
-            }
-        }*/
-        
-        fun createDocDialog(){
-            val dialogView = LayoutInflater.from(this).inflate(R.layout.create_doc, null)
-            val dialog = AlertDialog.Builder(this)
-                .setView(dialogView)
-                .create()
-            val titleDoc = dialogView.findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.title)
-            val contentDoc = dialogView.findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.content)
-            val workerDoc = dialogView.findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.worker)
-            val outputDoc = dialogView.findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.outputname)
-            val generateBtn = dialogView.findViewById<Button>(R.id.generatedoc)
-
-            generateBtn.setOnClickListener {
-                // Define o caminho para salvar o PDF na pasta Downloads
-                val pdfPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString() + "/"+outputDoc.text.toString()+".pdf"
-                val file = File(pdfPath)
-
-                val writer = PdfWriter(file)
-                val pdfDoc = PdfDocument(writer)
-                val document = Document(pdfDoc)
-
-                document.setFontSize(20F)
-                document.add(Paragraph(titleDoc.text.toString()))
-
-                document.setFontSize(12F)
-                document.add(Paragraph(contentDoc.text.toString()))
-
-                document.close()
-
-                dialog.dismiss()
-            }
-            dialog.show()
-        }
-
         createDoc.setOnClickListener {
             createDocDialog()
         }
+    }
+
+    private fun contentVer(a: TextInputEditText, b: Boolean = false) : String{
+        if (a.text.isNullOrBlank() && !b){
+            return ""
+        } else if (a.text.isNullOrBlank() && b){
+            return "Exemplo"
+        } else{
+            return  a.text.toString()
+        }
+    }
+
+    private fun createDocDialog(){
+        val dialogView = LayoutInflater.from(this).inflate(R.layout.create_doc, null)
+        val dialog = AlertDialog.Builder(this)
+            .setView(dialogView)
+            .create()
+        val titleDoc = dialogView.findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.title)
+        val contentDoc = dialogView.findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.content)
+        val workerDoc = dialogView.findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.worker)
+        val outputDoc = dialogView.findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.outputname)
+        val generateBtn = dialogView.findViewById<Button>(R.id.generatedoc)
+
+        generateBtn.setOnClickListener {
+            // Define o caminho para salvar o PDF na pasta Downloads
+            val pdfPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString() + "/"+contentVer(outputDoc, true)+".pdf"
+            val file = File(pdfPath)
+
+            val writer = PdfWriter(file)
+            val pdfDoc = PdfDocument(writer)
+            val document = Document(pdfDoc)
+
+            document.setFontSize(20F)
+            document.add(Paragraph(contentVer(titleDoc)))
+
+            document.setFontSize(12F)
+            document.add(Paragraph(contentVer(contentDoc)))
+
+            document.setFontSize(12F)
+            document.add(Paragraph(contentVer(workerDoc)))
+
+            document.close()
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 }
