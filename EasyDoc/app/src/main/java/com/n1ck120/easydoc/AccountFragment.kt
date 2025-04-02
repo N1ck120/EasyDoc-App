b529cb9b209bdf1416b45a2fbca15c5efe2d34ec
N1ck120 <nickolasaugusto74@gmail.com>
2025-04-02 11:38:22 -0300

    Make logout button functional

--- EasyDoc/app/src/main/java/com/n1ck120/easydoc/AccountFragment.kt
+++ EasyDoc/app/src/main/java/com/n1ck120/easydoc/AccountFragment.kt
@@ -1,12 +1,16 @@
 package com.n1ck120.easydoc
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Toast
+import androidx.fragment.app.DialogFragment
 
 class AccountFragment : Fragment() {
 
@@ -17,9 +21,30 @@ class AccountFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_account, container, false)
         val logout = view.findViewById<ImageButton>(R.id.btnLogout)
+        val login = view.findViewById<Button>(R.id.btnLoginRedirect)
+
         logout.setOnClickListener {
-            Toast.makeText(this.context, "Fazendo logout", Toast.LENGTH_SHORT).show()
+            val dialogView = LayoutInflater.from(this.context).inflate(R.layout.exit_dialog, null)
+            val dialog = AlertDialog.Builder(this.context)
+                .setView(dialogView)
+                .create()
+            val exit = dialogView.findViewById<Button>(R.id.exit)
+            val cancel = dialogView.findViewById<Button>(R.id.cancel)
+            exit.setOnClickListener{
+                activity?.finish()
+            }
+            cancel.setOnClickListener{
+                dialog.dismiss()
+            }
+                dialog.show()
         }
+
+        login.setOnClickListener{
+            val intent = Intent(this.context,LoginActivity::class.java)
+            startActivity(intent)
+            activity?.finish()
+        }
+
         return view
     }
 }
--- EasyDoc/app/src/main/java/com/n1ck120/easydoc/SettingsFragment.kt
+++ EasyDoc/app/src/main/java/com/n1ck120/easydoc/SettingsFragment.kt
@@ -4,10 +4,15 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.material.materialswitch.MaterialSwitch
 
 
 class SettingsFragment : Fragment() {
@@ -19,10 +24,21 @@ class SettingsFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
         val github = view.findViewById<ImageButton>(R.id.github)
+        val teste = view.findViewById<MaterialSwitch>(R.id.teste)
+        
         github.setOnClickListener {
             val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/N1ck120/EasyDoc-App"))
             startActivity(browserIntent)
         }
+
+        teste.setOnCheckedChangeListener { compoundButton, b ->
+            if (teste.isChecked){
+                Toast.makeText(this.context, "Ativado", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this.context, "Desativado", Toast.LENGTH_SHORT).show()
+            }
+        }
+
         return view
     }
 }
