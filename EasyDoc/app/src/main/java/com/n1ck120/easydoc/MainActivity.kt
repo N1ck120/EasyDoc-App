37efe17ba7f97cb6c7f9dbbc6305d999fc0764be
N1ck120 <nickolasaugusto74@gmail.com>
2025-04-04 23:21:50 -0300

    Switch to BottomNavigationView and set custom app icon

--- EasyDoc/app/src/main/java/com/n1ck120/easydoc/MainActivity.kt
+++ EasyDoc/app/src/main/java/com/n1ck120/easydoc/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.n1ck120.easydoc
 
 import android.app.AlertDialog
+import android.content.ClipData.Item
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import android.os.Environment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.widget.Button
-import android.widget.ImageButton
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.component1
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -17,6 +23,7 @@ import com.itextpdf.kernel.pdf.PdfDocument
 import com.itextpdf.kernel.pdf.PdfWriter
 import com.itextpdf.layout.Document
 import androidx.fragment.app.commit
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.itextpdf.layout.element.Paragraph
 import java.io.File
 
@@ -31,40 +38,63 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         val createDoc = findViewById<FloatingActionButton>(R.id.floatingActionButton)
-        val account = findViewById<ImageButton>(R.id.btnAccount)
-        val docs = findViewById<ImageButton>(R.id.btnDocs)
-        val settings = findViewById<ImageButton>(R.id.btnSettings)
-        val home = findViewById<ImageButton>(R.id.btnHome)
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
         createDoc.setOnClickListener {
             createDocDialog()
         }
 
-        home.setOnClickListener {
-            supportFragmentManager.commit {
-                setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
-                replace(R.id.fragmentContainerView, HomeFragment())
-            }
-        }
+        bottomNavigation.setOnItemSelectedListener { item ->
+            val homeBtn = bottomNavigation.menu.findItem(R.id.item_1)
+            val docsBtn = bottomNavigation.menu.findItem(R.id.item_2)
+            val accountBtn = bottomNavigation.menu.findItem(R.id.item_3)
+            val aettingsBtn = bottomNavigation.menu.findItem(R.id.item_4)
+            homeBtn.setIcon(R.drawable.round_home_24)
+            docsBtn.setIcon(R.drawable.round_insert_drive_file_24)
+            accountBtn.setIcon(R.drawable.round_account_circle_24)
+            aettingsBtn.setIcon(R.drawable.round_settings_24)
 
-        docs.setOnClickListener {
-            supportFragmentManager.commit {
-                setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
-                replace(R.id.fragmentContainerView, DocsFragment())
-            }
-        }
+            when(item.itemId) {
+                R.id.item_1 -> {
+                    supportFragmentManager.commit {
+                        setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+                        replace(R.id.fragmentContainerView, HomeFragment())
+                    }
 
-        account.setOnClickListener {
-            supportFragmentManager.commit {
-                setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
-                replace(R.id.fragmentContainerView, AccountFragment())
-            }
-        }
+                    item.setIcon(R.drawable.outline_home_24)
+                    // Respond to navigation item 1 click
+                    true
 
-        settings.setOnClickListener {
-            supportFragmentManager.commit {
-                setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
-                replace(R.id.fragmentContainerView, SettingsFragment())
+                }
+                R.id.item_2 -> {
+                    supportFragmentManager.commit {
+                        setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+                        replace(R.id.fragmentContainerView, DocsFragment())
+                    }
+
+                    item.setIcon(R.drawable.outline_insert_drive_file_24)
+                    // Respond to navigation item 2 click
+                    true
+                }
+                R.id.item_3 -> {
+                    supportFragmentManager.commit {
+                        setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+                        replace(R.id.fragmentContainerView, AccountFragment())
+                    }
+                    item.setIcon(R.drawable.outline_account_circle_24)
+                    // Respond to navigation item 2 click
+                    true
+                }
+                R.id.item_4 -> {
+                    supportFragmentManager.commit {
+                        setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+                        replace(R.id.fragmentContainerView, SettingsFragment())
+                    }
+                    item.setIcon(R.drawable.outline_settings_24)
+                    // Respond to navigation item 2 click
+                    true
+                }
+                else -> false
             }
         }
     }
