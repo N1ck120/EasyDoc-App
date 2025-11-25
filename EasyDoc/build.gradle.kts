2213a8692558ffd8f19a2f57fc757fceed3aebc1
N1ck120 <85962372+N1ck120@users.noreply.github.com>
2025-11-25 19:48:19 -0300

    Update dependencies

--- EasyDoc/build.gradle.kts
+++ EasyDoc/build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    id("com.google.devtools.ksp") version "2.3.2" apply false
+    id("com.google.devtools.ksp") version "2.3.3" apply false
     //kotlinx.serialization
     kotlin("jvm") version "2.2.21" // or kotlin("multiplatform") or any other kotlin plugin
     kotlin("plugin.serialization") version "2.2.21"
